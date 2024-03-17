5113ed394bad470e42d756793df8cbe0fab1b58a
Favi Derreira <davisanto@digix.com.br>
2024-03-17 15:39:44 -0400

    funcionando, precisando ajeitar o código e finalizar classe de testes

--- src/test/java/com/example/casapopular/api/ProcessoDeSelecaoRestTest.java
+++ src/test/java/com/example/casapopular/api/ProcessoDeSelecaoRestTest.java
@@ -4,6 +4,7 @@ import com.example.casapopular.CasaPopularApplication;
 import com.example.casapopular.dominio.repositorio.FamiliaRepositorio;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -31,13 +36,12 @@ public class ProcessoDeSelecaoRestTest {
             new PostgreSQLContainer("postgres:11.1")
                     .withDatabaseName("integration-tests-db")
                     .withUsername("sa")
-                    .withPassword("sa")
-                    .withInitScript("data_tests.sql");
+                    .withPassword("");
 
     static class Initializer
             implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            postgreSQLContainer.start(); // Start the container before obtaining the JDBC URL
+            postgreSQLContainer.start();
             TestPropertyValues.of(
                     "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
                     "spring.datasource.username=" + postgreSQLContainer.getUsername(),
@@ -49,8 +53,22 @@ public class ProcessoDeSelecaoRestTest {
     @Autowired
     private FamiliaRepositorio familiaRepositorio;
 
+    @BeforeEach
+    void setUp() throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        );
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("INSERT INTO Familia(nome) VALUES ('Santos');");
+    }
+
+    //TODO: transferir isso pra uma classe teste de integração e criar método privado para popular banco a partir do script data_tests.sql
+
     @Test
     void deveCriarProcessoDeSelecao() throws IOException {
+        familiaRepositorio.findAll();
         assertNotNull(familiaRepositorio);
         // Add your test logic here
     }
